ceee0f8f0b0331a0a2c8d199d1f3574401ff8582
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-07-10 22:53:19 -0700

    modifying main

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import "github.com/jung-kurt/gofpdf"
 
 func main() {
-  pdf := gofpdf.New("P", "mm", "A4", "")
+  pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+  w, h := pdf.GetPageSize()
+  // we print the width and height to not get an error
+  fmt.Printf("width=%v", "height=%v\n", w, h)
   pdf.AddPage()
-  pdf.SetFont("Arial", "B", 16)
+
+
+
+  pdf.AddPage()
+  pdf.SetFont("arial", "B", 38)
   pdf.Cell(40, 10, "Hello, world")
-  err := pdf.OutputFileAndClose("hello.pdf")
+  err := pdf.OutputFileAndClose("p1.pdf")
+  if err != nil {
+    panic(err)
+  }
 }
