cc6abab5fad71b7da6b58fa52759f492a864be57
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-07-11 15:52:39 -0700

    adding polygons, foodonate color

--- main.go
+++ main.go
@@ -5,48 +5,71 @@ import (
   "github.com/jung-kurt/gofpdf"
 )
 
+type PDF struct {
+  fpdf *gofpdf.Fpdf
+  x, y float64
+}
+
+func(p * PDF) Move(xDelta, yDelta float64) {
+  p.x,
+}
+
 func main() {
-  pdf := gofpdf.New(gofpdf.OrientationPortrait, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
+  pdf := gofpdf.New(gofpdf.OrientationLandscape, gofpdf.UnitPoint, gofpdf.PageSizeLetter, "")
   w, h := pdf.GetPageSize()
-  // we print the width and height to not get an error
-  fmt.Printf("width=%v", "height=%v\n", w, h)
+  // // we print the width and height to not get an error
+  // fmt.Printf("width=%v", "height=%v\n", w, h)
   pdf.AddPage()
 
-  pdf.MoveTo(0, 0)
-  pdf.SetFont("arial", "B", 30)
-  // to get exact fontsize
-  _, lineHt := pdf.GetFontSize()
-  pdf.SetTextColor(255, 0, 0)
-  pdf.Text(0, lineHt, "Hello, world")
-  pdf.MoveTo(0, lineHt*2.0)
-
-
-  pdf.SetFont("times", "", 18)
-  pdf.SetTextColor(100, 100, 100)
-  _, lineHt = pdf.GetFontSize()
-  pdf.MultiCell(0, lineHt, "Foodonate would like to extend the highest appreciation, on behalf of every member of the team, to", gofpdf.BorderNone, gofpdf.AlignRight, false)
-
-  // Shapes
-  pdf.SetFillColor(0, 255, 0)
-  pdf.SetDrawColor(0, 0, 255)
-  pdf.Rect(10, 100, 100, 100, "FD")
-  pdf.SetFillColor(100, 200, 200)
+  // top banner
+  pdf.SetFillColor(61,186,213)
   pdf.Polygon([]gofpdf.PointType{
-    {110, 250},
-    {160, 300},
-    {110, 350},
-    {60, 300},
-    {70, 230},
+    {0, 0},
+    {0, h / 9.0},
+    {w, 0},
+    {w, 0},
   }, "F")
+  pdf.SetFillColor(41, 166, 193)
+  pdf.Polygon([]gofpdf.PointType{
+    {0, 0},
+    {w, 0},
+    {w, h / 9.0},
+  }, "F")
+  // pdf.MoveTo(0, 0)
+  // pdf.SetFont("arial", "B", 30)
+  // // to get exact fontsize
+  // _, lineHt := pdf.GetFontSize()
+  // pdf.SetTextColor(255, 0, 0)
+  // pdf.Text(0, lineHt, "Hello, world")
+  // pdf.MoveTo(0, lineHt*2.0)
+  //
+  //
+  // pdf.SetFont("times", "", 18)
+  // pdf.SetTextColor(100, 100, 100)
+  // _, lineHt = pdf.GetFontSize()
+  // pdf.MultiCell(0, lineHt, "Foodonate would like to extend the highest appreciation, on behalf of every member of the team, to", gofpdf.BorderNone, gofpdf.AlignRight, false)
+  //
+  // // Shapes
+  // pdf.SetFillColor(0, 255, 0)
+  // pdf.SetDrawColor(0, 0, 255)
+  // pdf.Rect(10, 100, 100, 100, "FD")
+  // pdf.SetFillColor(100, 200, 200)
+  // pdf.Polygon([]gofpdf.PointType{
+  //   {110, 250},
+  //   {160, 300},
+  //   {110, 350},
+  //   {60, 300},
+  //   {70, 230},
+  // }, "F")
+  //
+  // // printing Foodonate logo
+  // pdf.ImageOptions("images/FOODONATE.png", 275, 275, 92, 0, false, gofpdf.ImageOptions{
+  //   ReadDpi: true,
+  //   }, 0, "")
 
-  pdf.ImageOptions("images/FOODONATE.png", 275, 275, 92, 0, false, gofpdf.ImageOptions{
-    ReadDpi: true,
-    }, 0, "")
-
-  // Calling the grid
-  drawGrid(pdf)
-
-  err := pdf.OutputFileAndClose("p1.pdf")
+  // // Calling the grid
+  // // drawGrid(pdf)
+  err := pdf.OutputFileAndClose("cert.pdf")
   if err != nil {
     panic(err)
   }
@@ -58,14 +81,14 @@ func drawGrid(pdf *gofpdf.Fpdf) {
   pdf.SetTextColor(80, 80, 80)
   pdf.SetDrawColor(200, 200, 200)
   for x := 0.0; x < w; x = x + (w / 20.0) {
+    pdf.SetTextColor(200, 200, 200)
     pdf.Line(x, 0, x, h)
     _, lineHt := pdf.GetFontSize()
     pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
   }
   for y := 0.0; y < h; y = y + (w / 20.0) {
+    pdf.SetTextColor(80, 80, 80)
     pdf.Line(0, y, w, y)
     pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
-
-
   }
 }
